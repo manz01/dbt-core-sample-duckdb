16445276f22760ccdee2c4c388d556d7932749c4
Manzar Ahmed <manzar010@gmail.com>
2025-06-12 20:13:29 +0100

    Remove staging models and configurations for GO Sales data

--- models/go-sales/stg/t_stg_go_methods.sql
+++
@@ -1,3 +0,0 @@
-select  "Order method code" as order_method_code,  
-        "Order method type" as order_method_type
-from    {{ ref('t_raw_go_methods') }}
--- models/go-sales/stg/t_stg_go_retailers.sql
+++
@@ -1,5 +0,0 @@
-select  "Retailer code" as retailer_code, 
-        "Retailer name" as retailer_name, 
-        "Type" as type, 
-        "Country" as country
-from    {{ ref('t_raw_go_retailers') }}

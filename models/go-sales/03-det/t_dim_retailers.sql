3e77c848745d59c85728944fa03e9e6411016168
Manzar Ahmed <manzar010@gmail.com>
2025-06-23 16:04:16 +0100

    Refactor t_dim_retailers model to use SCD2 variables for start and end timestamps

--- models/go-sales/03-det/t_dim_retailers.sql
+++ models/go-sales/03-det/t_dim_retailers.sql
@@ -12,6 +12,8 @@ Program history:
 Date        Programmer             Description
 ----------  ---------------------  ---------------------------------------------
 2025-06-11  Manzar Ahmed           v0.01/Initial version
+2025-06-23  Manzar Ahmed           v0.02/Utilising SCD2 vars for start and end 
+                                   timestamps
 -------------------------------------------------------------------------------*/
 
 {{ config(
@@ -21,102 +23,102 @@ Date        Programmer             Description
     pre_hook = ["create sequence if not exists seq_dim_retailer_sk start 1 increment 1"]
 ) }}
 
--- Set start_ts with second precision
-{% do run_query("SET VARIABLE start_ts = (SELECT date_trunc('second', current_timestamp));") %}
--- Subtract 1 second from the stored timestamp
-{% do run_query("SET VARIABLE end_ts = (SELECT cast(getvariable('start_ts') AS timestamp) - INTERVAL 1 SECOND);") %}
-
-{%- set high_date = '9999-12-31 00:00:00' %}
+{% set vars = get_scd2_vars() %}
+{% set start_ts = vars.start_ts %}
+{% set end_ts = vars.end_ts %}
+{% set high_date = vars.high_date %}
 
 with current_data as (
-    select
-        retailer_code,
-        retailer_name,
-        type,
-        country,
-        md5(
-            coalesce(retailer_name, '') || '|' ||
-            coalesce(type, '') || '|' ||
-            coalesce(country, '')
-        ) as scd2_hash
-    from {{ ref('t_stg_go_retailers') }}
+    select  retailer_code,
+            retailer_name,
+            type,
+            country,
+            md5(
+                coalesce(retailer_name, '') || '|' ||
+                coalesce(type, '') || '|' ||
+                coalesce(country, '')
+                ) as scd2_hash
+    from    {{ ref('t_stg_go_retailers') }}
 )
 
 {% if is_incremental() %},
 
 existing_records as (
-    select
-        retailer_code,
-        retailer_name,
-        type,
-        country,
-        scd2_hash,
-        start_ts,
-        end_ts
-    from {{ this }}
-    where end_ts = ('{{ high_date }}')::timestamp
+    select      retailer_code,
+                retailer_name,
+                type,
+                country,
+                scd2_hash,
+                start_ts,
+                end_ts
+    from        {{ this }}
+    where       end_ts = ('{{ high_date }}')::timestamp
 ),
 
 ordered_changes as (
-    select
-        c.retailer_code,
-        c.retailer_name,
-        c.type,
-        c.country,
-        c.scd2_hash
-    from current_data c
-    left join existing_records e
-        on c.retailer_code = e.retailer_code
-    where e.retailer_code is null
-       or c.scd2_hash != e.scd2_hash
-    order by c.retailer_code
+    select      c.retailer_code,
+                c.retailer_name,
+                c.type,
+                c.country,
+                c.scd2_hash
+    from        current_data c
+    left join   existing_records e
+    on          c.retailer_code = e.retailer_code
+    where       e.retailer_code is null
+    or          c.scd2_hash != e.scd2_hash
+    order by    c.retailer_code
 ),
 
 changes as (
-    select
-        nextval('seq_dim_retailer_sk') as dim_retailer_sk,
-        oc.retailer_code,
-        oc.retailer_name,
-        oc.type,
-        oc.country,
-        oc.scd2_hash,
-        getvariable('start_ts') as start_ts,
-        ('{{ high_date }}')::timestamp as end_ts
-    from ordered_changes oc
+    select  nextval('seq_dim_retailer_sk') as dim_retailer_sk,
+            oc.retailer_code,
+            oc.retailer_name,
+            oc.type,
+            oc.country,
+            oc.scd2_hash,
+            ('{{ start_ts }}')::timestampas start_ts,
+            ('{{ high_date }}')::timestamp as end_ts
+    from    ordered_changes oc
 ),
 
 updates as (
-    select
-        null as dim_retailer_sk,
-        e.retailer_code,
-        e.retailer_name,
-        e.type,
-        e.country,
-        e.scd2_hash,
-        e.start_ts,
-        getvariable('end_ts')  as end_ts
-    from existing_records e
-    join changes c
-      on e.retailer_code = c.retailer_code
-    where e.end_ts = ('{{ high_date }}')::timestamp
+    select  e.dim_retailer_sk,
+            e.retailer_code,
+            e.retailer_name,
+            e.type,
+            e.country,
+            e.scd2_hash,
+            e.start_ts,
+            ('{{ end_ts }}')::timestamp  as end_ts
+    from    existing_records e
+    join    changes c
+    on      e.retailer_code = c.retailer_code
+    where   e.end_ts = ('{{ high_date }}')::timestamp
+),
+
+unchanged_history as (
+    select *
+    from {{ this }}
+    where end_ts != ('{{ high_date }}')::timestamp
 )
 
 select * from changes
 union all
 select * from updates
+union all
+select * from unchanged_history
 
 {% else %}
 
-select
-    nextval('seq_dim_retailer_sk') as dim_retailer_sk,
-    retailer_code,
-    retailer_name,
-    type,
-    country,
-    scd2_hash,
-    getvariable('start_ts')  as start_ts,
-    ('{{ high_date }}')::timestamp as end_ts
-from current_data
-order by retailer_code
+select      nextval('seq_dim_retailer_sk') as dim_retailer_sk,
+            retailer_code,
+            retailer_name,
+            type,
+            country,
+            scd2_hash,
+            ('{{ start_ts }}')::timestamp  as start_ts,
+            ('{{ high_date }}')::timestamp as end_ts
+from        current_data
+order by    retailer_code
 
 {% endif %}

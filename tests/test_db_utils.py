5129a029eac1010f37e4174ff3313e56ca975076
Manzar Ahmed <manzar010@gmail.com>
2025-06-23 19:41:32 +0100

    Add coverage.xml file for test coverage reporting and update mock patches in test_db_utils.py

--- tests/test_db_utils.py
+++ tests/test_db_utils.py
@@ -6,9 +6,9 @@ from shared_utils.db_utils import db_query
 from tests.test_data import mock_config_data
 
 
-@mock.patch("shared_utils.db_query.get_db_config")
-@mock.patch("shared_utils.db_query.mysql.connector.connect")
-@mock.patch("shared_utils.db_query.pd.read_sql")
+@mock.patch("shared_utils.db_utils.get_db_config")
+@mock.patch("shared_utils.db_utils.mysql.connector.connect")
+@mock.patch("shared_utils.db_utils.pd.read_sql")
 def test_db_query_success(mock_read_sql, mock_connect, mock_get_config):
     """Test db_query runs SQL and returns DataFrame correctly."""
     fake_config = mock_config_data["GOSales"]

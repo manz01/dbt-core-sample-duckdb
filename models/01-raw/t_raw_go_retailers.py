caa313300ad191292e75153496e6838def0b2685
Manzar Ahmed <manzar010@gmail.com>
2025-06-26 16:29:31 +0100

    Update to comply with flake8 rules E501, W391

--- models/01-raw/t_raw_go_retailers.py
+++ models/01-raw/t_raw_go_retailers.py
@@ -1,5 +1,5 @@
 """
---------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 Program:        t_raw_go_retailers
 Project:        duckdb-core-sample-go-sales
 Description:    Raw model for the GO Sales data go_retailers
@@ -7,21 +7,22 @@ Input(s):       msql go_sales.go_retailers
 Output(s):      raw.t_raw_go_retailers
 Author:         Manzar Ahmed
 First Created:  Jun 2025
---------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 Program history:
---------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 Date        Programmer             Description
-----------  ---------------------  ---------------------------------------------
+----------  ---------------------  --------------------------------------------
 2025-06-11  Manzar Ahmed           v0.01/Initial version
---------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 """
 
 from shared_utils.db_utils import db_query
 
+
 def model(dbt, session):  # pylint: disable=unused-argument # noqa: ARG001
     """
-    Executes a SQL query against the 'GOSales' database using the provided SQL 
-    from dbt configuration,and returns the result as a DataFrame.
+    Executes a SQL query against the 'GOSales' database using the provided SQL
+    from dbt configuration, and returns the result as a DataFrame.
     """
     sql = dbt.config.get('sql')
     df = db_query(sql, 'GOSales')
